399f3475848319e0ce6d9db72efd793d073963d2
xlufe <3579078141@qq.com>
2025-06-05 14:30:20 +0800

    1.3

---
+++ 6-5 PWM驱动直流电机/Hardware/Motor.h
@@ -0,0 +1,11 @@
+#ifndef __MOTOR_H
+#define __MOTOR_H
+
+void Motor_Init(void);
+void Motor_SetSpeed(int8_t Speed);
+void Motor_IN(void);
+void Motor_OUT(void);
+void Motor_R(void);
+void Motor_L(void);
+
+#endif
